5e308b547fa6e4d10f01c0a2d4fb0dd578f2f8f8
ash0tych <ash0tykit10@gmail.com>
2021-07-27 10:50:42 +0300

    It can move now!

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,3 +0,0 @@
-
-<
-ChessGame.py,c\b\cb783d6c8a43474e64470ce996e30a090bf4644a
--- ChessGame.py
+++ ChessGame.py
@@ -24,9 +24,6 @@ class EmptyFigure(ChessFigure):
     def __str__(self):
         return '. '
 
-    def get_moves(self, board, xy):
-        return []
-
 
 class Pawn(ChessFigure):
     def __str__(self):
@@ -52,7 +49,7 @@ class Pawn(ChessFigure):
         return moves
 
     def draw(self, screen_name, xy):
-        if self.image == None:
+        if self.image is None:
             if self.color == Colors.BLACK:
                 self.image = pygame.image.load(os.path.join(img_folder, 'bP.png'))
             else:
@@ -63,7 +60,7 @@ class Pawn(ChessFigure):
         screen_name.blit(self.image, self.rect)
 
     def clicked(self, screen_name, board, xy):
-        moves = self.get_moves(board, (xy[0] // 80, xy[1] // 80))
+        moves = self.get_moves(board, (xy[0], xy[1]))
         for pos in moves:
             img = pygame.image.load(os.path.join(img_folder, 'border.png'))
             rect = img.get_rect()
@@ -76,7 +73,7 @@ class ChessBoard(object):
     def __init__(self):
         self.board = [[EmptyFigure(Colors.EMPTY)] * 8 for _ in range(8)]
         self.board[1][0] = Pawn(Colors.BLACK)
-        self.board[2][1] = Pawn(Colors.WHITE)
+        self.board[7][1] = Pawn(Colors.WHITE)
 
     def __str__(self):
         result = '  a b c d e f g h\n'
@@ -90,6 +87,7 @@ class ChessBoard(object):
 
     def move(self, xy_before, xy_after):
         moves = self.get_moves(xy_before)
+        print(moves)
         if moves.count(xy_after) > 0:
             self.board[xy_after[1]][xy_after[0]] = self.board[xy_before[1]][xy_before[0]]
             self.board[xy_before[1]][xy_before[0]] = EmptyFigure(Colors.EMPTY)
@@ -104,14 +102,22 @@ class ChessBoard(object):
                     rect.topleft = (i * 80, j * 80)
                     screen_name.blit(image, rect)
 
+                else:
+                    image = pygame.Surface((80, 80))
+                    image.fill(BROWN)
+                    rect = image.get_rect()
+                    rect.topleft = (i * 80, j * 80)
+                    screen_name.blit(image, rect)
+
                 if type(self.board[i][j]) != EmptyFigure:
                     self.board[i][j].draw(screen_name, (i, j))
 
     def check_click(self, pos):
-        return type(self.board[pos[1] // 80][pos[0] // 80]) != EmptyFigure
+        return type(self.board[pos[1]][pos[0]]) != EmptyFigure
 
     def draw_borders(self, screen_name, pos):
-            self.board[pos[1] // 80][pos[0] // 80].clicked(screen_name, self.board, pos)
+            self.board[pos[1]][pos[0]].clicked(screen_name, self.board, pos)
+
 
 c_board = ChessBoard()
 pygame.init()
@@ -122,6 +128,9 @@ clock = pygame.time.Clock()
 screen.fill((255, 255, 255))
 c_board.draw_all(screen)
 pygame.display.update()
+
+is_screen_clicked = False
+last_click = None
 
 # Цикл игры
 running = True
@@ -134,12 +143,19 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        if event.type == pygame.MOUSEBUTTONDOWN:  # Better to seperate to a new if statement aswell, since there's more buttons that can be clicked and makes for cleaner code.
-            if event.button == 1 and c_board.check_click(event.pos):
-                screen.fill((255, 255, 255))
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1 and c_board.check_click((event.pos[0] // 80, event.pos[1] // 80)) and is_screen_clicked == False:
                 c_board.draw_all(screen)
-                c_board.draw_borders(screen, event.pos)
+                c_board.draw_borders(screen, (event.pos[0] // 80, event.pos[1] // 80))
                 pygame.display.update()
+                is_screen_clicked = True
+                last_click = event.pos
+
+            elif event.button == 1 and is_screen_clicked:
+                c_board.move((last_click[0] // 80, last_click[1] // 80), (event.pos[0] // 80, event.pos[1] // 80))
+                c_board.draw_all(screen)
+                pygame.display.update()
+                is_screen_clicked = False
 
             else:
                 screen.fill((255, 255, 255))
